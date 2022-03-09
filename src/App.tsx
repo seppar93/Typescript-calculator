ccc7ac12869ba68952befc4f697e418f39203f38
Sepehr Parirokh <sepehrparirokh@gmail.com>
2022-03-09 18:58:30 -0500

    reducer type setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,49 @@
-import React, { useState } from 'react';
+import React, { useReducer, } from 'react';
+
+
+type InitialState = {
+  currentOperand:number,
+  previousOperand:number,
+  operation:string
+}
+
+type ActionType = | {type: 'ADD_DIGIT', data: number}
+| {type: 'DELETE_DIGIT', data: number}
+| {type: 'CHOOSE_OPERATION', data: string}
+| {type: 'EVALUATE', data: number}
+
+| {type: 'CLEAR'}
+
+
+
+
+
+function reducer (state:InitialState, action:ActionType):InitialState {
+
+  switch(action.type) {
+    case 'ADD_DIGIT':
+      return {
+        ...state,
+      }
+    default: return state
+  }
+}
+
+const initialState = {
+  currentOperand: 0,
+  previousOperand: 0,
+  operation: ''
+
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
   
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">123,123 *</div>
-        <div className="current-operand">123241</div>
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
           </div>
 
           <button className="span-two">AC</button>
