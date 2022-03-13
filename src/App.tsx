8ce8e5a11636179357c6a72c6ef144b1ac37d74c
Sepehr Parirokh <sepehrparirokh@gmail.com>
2022-03-13 19:36:35 -0400

    digit format

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ type InitialState = {
   currentOperand: string | null;
   previousOperand: string | null;
   operation: string | null;
-  overwrite:boolean;
+  overwrite: boolean;
 };
 
 export type ActionType =
@@ -19,131 +19,146 @@ export type ActionType =
   | { type: 'CLEAR' }
   | { type: 'EVALUATE' };
 
-
 function reducer(state: InitialState, action: ActionType): InitialState {
   switch (action.type) {
-      case 'ADD_DIGIT':
-        if (state.overwrite) {
-          return {
-            ...state,
-            currentOperand: action.payload.digit,
-            overwrite: false,
-          }
-        }
-        if (action.payload.digit === "0" && state.currentOperand === "0") {
-          return state
-        }
-        if(state.currentOperand != null) {
-
-          if (action.payload.digit === "." && state.currentOperand.includes(".")) {
-            return state
-          }
-        }
-  
+    case 'ADD_DIGIT':
+      if (state.overwrite) {
         return {
           ...state,
-          currentOperand: `${state.currentOperand || ""}${action.payload.digit}`,
-        }
-      case 'CHOOSE_OPERATION':
-        if (state.currentOperand == null && state.previousOperand == null) {
-          return state
-        }
-  
-        if (state.currentOperand == null) {
-          return {
-            ...state,
-            operation: action.payload.operation,
-          }
-        }
-  
-        if (state.previousOperand == null) {
-          return {
-            ...state,
-            operation: action.payload.operation,
-            previousOperand: state.currentOperand,
-            currentOperand: null,
-          }
-        }
-  
-        return {
-          ...state,
-          previousOperand: evaluate(state),
-          operation: action.payload.operation,
-          currentOperand: null,
-        }
-      case 'CLEAR':
-        return state
-      case 'DELETE_DIGIT':
-
-        if (state.overwrite) {
-          return {
-            ...state,
-            overwrite: false,
-            currentOperand: null,
-          }
-        }
-        if (state.currentOperand == null) return state
-        if (state.currentOperand.length === 1) {
-          return { ...state, currentOperand: null }
-        }
-  
-        return {
-          ...state,
-          currentOperand: state.currentOperand.slice(0, -1),
-        }
-      case 'EVALUATE':
+          currentOperand: action.payload.digit,
+          overwrite: false,
+        };
+      }
+      if (action.payload.digit === '0' && state.currentOperand === '0') {
+        return state;
+      }
+      if (state.currentOperand != null) {
         if (
-          state.operation == null ||
-          state.currentOperand == null ||
-          state.previousOperand == null
+          action.payload.digit === '.' &&
+          state.currentOperand.includes('.')
         ) {
-          return state
+          return state;
         }
-  
+      }
+
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ''}${action.payload.digit}`,
+      };
+    case 'CHOOSE_OPERATION':
+      if (state.currentOperand == null && state.previousOperand == null) {
+        return state;
+      }
+
+      if (state.currentOperand == null) {
         return {
           ...state,
-          overwrite: true,
-          previousOperand: null,
-          operation: null,
-          currentOperand: evaluate(state),
-        }
-    }
+          operation: action.payload.operation,
+        };
+      }
+
+      if (state.previousOperand == null) {
+        return {
+          ...state,
+          operation: action.payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null,
+        };
+      }
+
+      return {
+        ...state,
+        previousOperand: evaluate(state),
+        operation: action.payload.operation,
+        currentOperand: null,
+      };
+    case 'CLEAR':
+      return state;
+
+    case 'DELETE_DIGIT':
+      if (state.overwrite) {
+        return {
+          ...state,
+          overwrite: false,
+          currentOperand: null,
+        };
+      }
+      if (state.currentOperand == null) return state;
+      if (state.currentOperand.length === 1) {
+        return { ...state, currentOperand: null };
+      }
+
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1),
+      };
+    case 'EVALUATE':
+      if (
+        state.operation == null ||
+        state.currentOperand == null ||
+        state.previousOperand == null
+      ) {
+        return state;
+      }
+
+      return {
+        ...state,
+        overwrite: true,
+        previousOperand: null,
+        operation: null,
+        currentOperand: evaluate(state),
+      };
+  }
 }
 
-function evaluate({currentOperand, previousOperand, operation}: InitialState): string {
+function evaluate({
+  currentOperand,
+  previousOperand,
+  operation,
+}: InitialState): string {
   let computation = 0;
-  if(currentOperand !== null && previousOperand !== null) {
-    const prev = parseFloat(previousOperand)
-    const current = parseFloat(currentOperand)
-    if(isNaN(prev) || isNaN(current)) return ''
-    switch(operation) {
-      case "+": 
-      computation = prev+ current
-      break
-      case "-":
-        computation = prev- current
-        break
+  if (currentOperand !== null && previousOperand !== null) {
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return '';
+    switch (operation) {
+      case '+':
+        computation = prev + current;
+        break;
+      case '-':
+        computation = prev - current;
+        break;
 
-      case "*": 
-        computation = prev * current
-        break
-      case "/": 
-      computation = prev /current
-      break
+      case '*':
+        computation = prev * current;
+        break;
+      case '/':
+        computation = prev / current;
+        break;
     }
   }
-  return computation.toString()
-
+  return computation.toString();
 }
 
 const initialState = {
   currentOperand: null,
   previousOperand: null,
   operation: null,
-  overwrite: false
+  overwrite: false,
 };
 
+const INTEGER_FORMATTER = new Intl.NumberFormat('en-us', {
+  maximumFractionDigits: 0
+})
+function formatOperand(operand:string|null) {
+  if(operand !== null) {
 
+    if (operand == null) return 
+    const [integer,decimal] = operand.split('.')
+    if(decimal == null) return INTEGER_FORMATTER.format(integer)
+    return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
+  }
+}
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -154,14 +169,14 @@ function App() {
   return (
     <div className='calculator-grid'>
       <div className='output'>
-        <div className='previous-operand'> {state.previousOperand}</div>
-        <div className='current-operand'>{state.currentOperand}</div>
+        <div className='previous-operand'> {formatOperand(state.previousOperand)} {state.operation} </div>
+        <div className='current-operand'>{formatOperand(state.currentOperand)}</div>
       </div>
 
-      <button className='span-two' onClick={() => dispatch({ type: 'CLEAR' })}>
+      <button className='span-two' onClick={() => dispatch({ type: 'DELETE_DIGIT' })}>
         AC
       </button>
-      <button>DEL</button>
+      <button onClick={() => dispatch({ type: 'CLEAR' })}>DEL</button>
       <OperationButton operation='÷' dispatch={dispatch} />
       <DigitButton dispatch={dispatch} digit='1' />
       <DigitButton dispatch={dispatch} digit='2' />
@@ -178,7 +193,12 @@ function App() {
       <DigitButton digit='.' dispatch={dispatch} />
       <DigitButton digit='0' dispatch={dispatch} />
 
-      <button className='span-two'>=</button>
+      <button
+        onClick={() => dispatch({ type: 'EVALUATE' })}
+        className='span-two'
+      >
+        =
+      </button>
     </div>
   );
 }
--- src/components/OperationButton.tsx
+++ src/components/OperationButton.tsx
@@ -1,17 +1,21 @@
-import React, { Dispatch } from 'react'
-import { ActionType } from '../App'
-
+import React, { Dispatch } from 'react';
+import { ActionType } from '../App';
 
 type OperationButtonProps = {
-  dispatch: Dispatch<ActionType>,
-  operation: string
-}
+  dispatch: Dispatch<ActionType>;
+  operation: string;
+};
 
-const OperationButton = ({dispatch, operation}:OperationButtonProps ) => {
-    
+const OperationButton = ({ dispatch, operation }: OperationButtonProps) => {
   return (
-      <button onClick={() => dispatch({type:'CHOOSE_OPERATION', payload:{operation}})}>{operation}</button>
-  )
-}
+    <button
+      onClick={() =>
+        dispatch({ type: 'CHOOSE_OPERATION', payload: { operation } })
+      }
+    >
+      {operation}
+    </button>
+  );
+};
 
-export default OperationButton
+export default OperationButton;
